5bedeb847cae63913a38169af14469d9d9d2b64d
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-24 14:02:06 +0100

    adds the base project

---
+++ atarihelpers/__init__.py
@@ -0,0 +1,1 @@
+from .helpers import process_state
---
+++ atarihelpers/helpers.py
@@ -0,0 +1,18 @@
+import numpy
+import cv2
+
+
+# TODO we can add curated crops for every single environment
+def process_state(
+    state: numpy.array,
+    image_size: int = 84,
+    grayscale: bool = True,
+    resize: bool = True,
+) -> numpy.array:
+    if grayscale:
+        state = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
+
+    if resize:
+        state = cv2.resize(state, (image_size, image_size))
+
+    return state
